371a3305c2f853d6ca3f111757c231b79af917d4
baxterromero44 <89486675+baxterromero44@users.noreply.github.com>
2025-02-04 16:46:34 -0700

    Set up database
    Thanks Mike

--- server/index.js
+++ server/index.js
@@ -8,14 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(authRouter);
 
-<<<<<<< Updated upstream
-const DB = 
-    "mongodb+srv://mise3389:nqMGBHs8fgtS4u0J@cluster1.d6p3k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
-=======
 
 // CHANGE USERNAME AND PASSWORD FOR YOUR OWN!
 const DB = "mongodb+srv://baxter:baxter@cluster1.d6p3k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
->>>>>>> Stashed changes
 
 mongoose
     .connect(DB)
