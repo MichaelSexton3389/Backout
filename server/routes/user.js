c1e0ee38b020c41bf4b7c243fed4cdd5a2c5dab0
arsi6998 <97605491+arsi6998@users.noreply.github.com>
2025-02-25 15:18:07 -0700

    Update profile photo router added

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,40 @@
 const express = require("express");
 const userRouter = express.Router();
 const User = require("../models/user");
+const authMiddleware= require("../middleware/userAuth")
 
 userRouter.get("/users", async (req, res) => {
     try {
-        const users = await User.find({}, { name: 1, _id: 1 }); // Fetch only names and IDs
+        const users = await User.find({}, { name: 1, _id: 1, profile_picture: 1 }); // Fetch only names and IDs
         res.json(users);
     } catch (err) {
         res.status(500).json({ error: err.message });
+    }
+});
+
+
+
+userRouter.put('/update-photo', authMiddleware, async (req, res) => {
+    try {
+        const { userId, photoUrl } = req.body;
+
+        if (!userId || !photoUrl) {
+            return res.status(400).json({ message: 'Missing userId or photoUrl' });
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { profile_picture: photoUrl },
+            { new: true }
+        );
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.json({ message: 'Profile photo updated successfully', user: updatedUser });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
     }
 });
 
